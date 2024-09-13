b2bdb5e77db87f5a00da7595a2b1c0a0fc46d8f6
Hugo Perdigão <hugopq@gmail.com>
2024-09-14 00:06:16 +0100

    base

---
+++ action.js
@@ -0,0 +1,36 @@
+function getAllPosts()
+{
+    //ex1
+}
+
+function getPostData()
+{
+    //ex2
+}
+
+function getAllTasks()
+{
+    //ex3
+}
+
+function getCompletedTasks()
+{
+    //ex4
+}
+
+function getAlbumCount()
+{
+    //ex5
+}
+
+function getFirstAlbumPicture()
+{
+    //ex6
+}
+
+function getAlbumData()
+{
+    //ex7
+}
+
+
