a51ea3220bfa2b1c46197227acab965ce3939fd8
Hugo Perdigão <hugopq@gmail.com>
2024-09-14 00:36:37 +0100

    ex2 OK

--- action.js
+++ action.js
@@ -18,6 +18,24 @@ function getAllPosts()
 function getPostData()
 {
     //ex2
+    let txt_id = document.getElementById('txt_post_id');
+    if(txt_id.value == "")
+    {
+        alert('introduza o id');
+        return;
+    }
+    let url = 'https://jsonplaceholder.typicode.com/posts/' + txt_id.value;
+    let con = document.getElementsByClassName('console')[0];
+    fetch(url)
+        .then(function(response){
+            return response.json();            
+        })
+        .then(function(myJson){
+            let jsonText = JSON.stringify(myJson);
+            let myP = document.createElement('p');
+            myP.innerText = jsonText;            
+           con.insertBefore(myP, con.firstChild);
+        });
 }
 
 function getAllTasks()
